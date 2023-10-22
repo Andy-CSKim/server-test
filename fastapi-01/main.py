6b356d6485aa43ecf40867df5946f50139180bca
Andy-CSKim <andy.cskim@gmail.com>
2023-10-22 11:31:16 +0900

    remove query

--- fastapi-01/main.py
+++ fastapi-01/main.py
@@ -8,8 +8,4 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/query")
-async def items(skip: int = 0, limit: int = 10):
-    return {"message": "Items", "skip": skip, "limit": limit}
-
 app.mount("/", StaticFiles(directory="static", html=True), name="satic")
