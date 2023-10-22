8741ef7d0348a716e2018f59dcfb09ae5c1d4e66
Andy-CSKim <andy.cskim@gmail.com>
2023-10-22 11:50:50 +0900

    add cvtLength@fastapi

--- fastapi-01/main.py
+++ fastapi-01/main.py
@@ -8,4 +8,14 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/api/length/{value}")
+async def length(value: int, unit: str):
+    if unit == "inch":
+        return str(value * 2.54) + " inch"
+    elif unit == "feet":
+        return str(value * 0.3048) + " feet"
+    else:
+        return "invalid unit"
+
+# Mounting static web page
 app.mount("/", StaticFiles(directory="static", html=True), name="satic")
