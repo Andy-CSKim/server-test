77fef3d2e3397701d1d6815551474dcb92fee410
Andy-CSKim <andy.cskim@gmail.com>
2023-10-22 11:02:23 +0900

    add cvtLength

--- spring-01/src/main/java/com/blockki/spring01/controller/APIcontroller.java
+++ spring-01/src/main/java/com/blockki/spring01/controller/APIcontroller.java
@@ -1,12 +1,27 @@
 package com.blockki.spring01.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class APIcontroller {
-    @GetMapping("/hello")
+    @GetMapping("/hello")  // http://localhost:8090/hello : path parameter
     public String hello() {
         return "Hello World!";
+    }
+
+    // http://localhost:8090/length?unit=inch : /length is path parameter --> @PathVariable
+    // ?unit=inch is query parameter --> @RequestParam
+    @GetMapping("/api/length/{value}")
+    public String cvtLength(@PathVariable int value, @RequestParam String unit) {
+        if (unit.equals("inch")) {
+            return value / 2.54 + " inch";
+        }
+        else if (unit.equals("feet")) {
+            return value * 30.48 + " feet";
+        }
+        else {
+            return "invalid unit";
+        }
+
     }
 }
